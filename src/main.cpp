cec284962d8f3853bb9dd3949f3b16965c2fc689
nabijaczleweli <nabijaczleweli@gmail.com>
2016-05-07 23:02:15 +0200

    Don't act like you're configuring anything

--- src/main.cpp
+++ src/main.cpp
@@ -31,12 +31,14 @@
 using namespace std;
 
 
+static void credit();
 static pair<string, int> check_config();
 static void init_app(application & app, int argc, char * argv[]);
 static void init_deps(application & app);
 
 
 int main(int argc, char * argv[]) {
+	credit();
 	{
 		const auto cfg_result = check_config();
 		if(cfg_result.second) {
@@ -55,6 +57,21 @@ int main(int argc, char * argv[]) {
 }
 
 
+static void credit() {
+	cout << "Detected OS: " << CIMPOLER_META_OS_NAME << "\n"
+	     << "Compiler: " << CIMPOLER_META_COMPILER_NAME << " version " << cimpoler_meta::version() << "\n"
+	     << "Used libraries:\n"
+	     << "  - SFML version " << SFML_VERSION_MAJOR << '.' << SFML_VERSION_MINOR << ": http://sfml-dev.org\n"
+	     << "  - audiere version " << audiere::GetVersion() << ": https://sourceforge.net/projects/audiere\n"
+	     << "  - cereal version " << CEREAL_VERSION << ": http://uscilab.github.io/cereal\n"
+	     << "  - cimpoler-meta version " << CIMPOLER_META_VERSION << ": https://github.com/nabijaczleweli/cimpoler-meta\n"
+	     << "  - Cpp-NBT version " << CPP_NBT_VERSION << ": https://github.com/nabijaczleweli/Cpp-NBT\n"
+	     << "  - jsonpp version " << JSONPP_VERSION << ": http://rapptz.github.io/jsonpp\n"
+	     << "  - seed11 version " << SEED11_VERSION << ": https://github.com/milleniumbug/seed11\n"
+	     << "  - whereami-cpp version " << WHEREAMI_CPP_VERSION << ": https://github.com/nabijaczleweli/whereami-cpp\n"
+	     << "\n\n";
+}
+
 static pair<string, int> check_config() {
 	if(!app_configuration.vsync && !app_configuration.FPS)
 		return {"VSync not enabled and FPS set to 0", -1};
@@ -63,20 +80,4 @@ static pair<string, int> check_config() {
 }
 
 static void init_app(application &, int, char * []) {}
-
-static void init_deps(application &) {
-	cout << "Initializing dependencies under " << CIMPOLER_META_OS_NAME << "...\n\n"
-
-	     << CIMPOLER_META_COMPILER_NAME " version " << cimpoler_meta::version() << " doesn\'t need initialization.\n"
-	     << "SFML version " << SFML_VERSION_MAJOR << '.' << SFML_VERSION_MINOR << " doesn\'t need initialization.\n"
-	     << "audiere version " << audiere::GetVersion() << " doesn\'t need initialization.\n"
-	     << "cereal version " << CEREAL_VERSION << " doesn\'t need initialization.\n"
-	     << "cimpoler-meta version " << CIMPOLER_META_VERSION << " doesn\'t need initialization.\n"
-	     << "Cpp-NBT version " << CPP_NBT_VERSION << " doesn\'t need initialization.\n"
-	     << "jsonpp version " << JSONPP_VERSION << " doesn\'t need initialization.\n"
-	     << "seed11 version " << SEED11_VERSION << " doesn\'t need initialization.\n"
-	     << "whereami-cpp version " << WHEREAMI_CPP_VERSION << " doesn\'t need initialization.\n"
-	     << "\n"
-
-	     << "All dependencies initialized.\n\n";
-}
+static void init_deps(application &) {}
